43bc8c4ad868461f5470cff3ce37893354d5f54a
Kaustubh <saikaustubh10@gmail.com>
2021-03-27 11:10:16 +0530

    added passport js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
+
+// passport.use(new GogleStrategy());
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there' })
