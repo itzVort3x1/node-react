4bf5954e008919acb9c7bb9af1621f4933b204e2
Kaustubh <saikaustubh10@gmail.com>
2021-03-28 08:46:05 +0530

    added google strategy

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const passport = require('passport');
 const GogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-// passport.use(new GogleStrategy());
+passport.use(new GogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+);
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
