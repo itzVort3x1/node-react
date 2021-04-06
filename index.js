a6f512165a3fbae068fbedeaa887a75ce754cc7b
Kaustubh <saikaustubh10@gmail.com>
2021-04-06 16:19:31 +0530

    changed script

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
+const path= require('path');
 require('./models/User');
 require('./services/passport');
 
@@ -34,12 +35,12 @@ require('./routes/billingRoutes')(app);
 if (process.env.NODE_ENV === 'production'){
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     //Express will serve up the index.html file
     // if it dosen't find the Route
-    const path= require('path');
+    
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     })
 }
 
